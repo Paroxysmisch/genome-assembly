a0396fb034a5621a5612a66f6a1aadf5b0843007
Yash Shah <yashshah04.03.2002@outlook.com>
2024-12-04 22:36:29 +0000

    Initial commit---Basic implementation of ArabidopsisDataset..

---
+++ arabidopsis.py
@@ -0,0 +1,61 @@
+import os
+import tarfile
+
+from dgl import load_graphs
+import torch
+from torch_geometric.data import Data, InMemoryDataset, download_google_url
+from torch_geometric.utils import from_dgl
+import pickle
+
+
+class ArabidopsisDataset(InMemoryDataset):
+    def __init__(
+        self,
+        root,
+        transform=None,
+        pre_transform=None,
+        pre_filter=None,
+    ):
+        super().__init__(root, transform, pre_transform, pre_filter)
+        self.load(self.processed_paths[0])
+
+    @property
+    def raw_file_names(self):
+        return ["arabidopsis-data.tar"]
+
+    @property
+    def processed_file_names(self):
+        return ["arabidopsis-data.pt"]
+
+    def download(self):
+        download_google_url(
+            id="1QqfLJcs0LYWx2tr1rYAwKO754OVEcHHM",
+            folder=self.raw_dir,
+            filename="arabidopsis-data.tar",
+        )
+
+    def process(self):
+        archive = tarfile.open(self.raw_dir + '/arabidopsis-data.tar')
+        archive.extractall(self.raw_dir)
+        archive.close()
+
+        # Read data into huge `Data` list.
+        data_list = []
+
+        chromosomes=[3, 4, 5]
+        dgls = [
+            "chr" + str(chromosome_number) + ".dgl" for chromosome_number in chromosomes
+        ]
+
+        for dgl in dgls:
+            (graph,), _ = load_graphs(self.raw_dir + '/' + dgl)
+            data = from_dgl(graph)
+
+            data_list.append(data)
+
+
+        self.save(data_list, self.processed_paths[0])
+
+
+test = ArabidopsisDataset(root="./arabidopsis-dataset")
+print(test[0])
---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pip install dgl -f https://data.dgl.ai/wheels/torch-2.4/cu124/repo.html
+pip install -r requirements

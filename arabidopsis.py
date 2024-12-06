127133e9aa97c7f8819a2e2537c0f35f4422adfd
Yash Shah <yashshah04.03.2002@outlook.com>
2024-12-06 20:38:01 +0000

    One-hot encoding of read into 2D tensor sample.

--- arabidopsis.py
+++ arabidopsis.py
@@ -1,9 +1,10 @@
 import tarfile
-
+import pickle
 import torch
 from dgl import load_graphs
 from torch_geometric.data import InMemoryDataset, download_google_url
 from torch_geometric.utils import from_dgl
+from torch.utils.data.dataset import Dataset
 
 
 class ArabidopsisDataset(InMemoryDataset):
@@ -57,6 +58,47 @@ class ArabidopsisDataset(InMemoryDataset):
 
         self.save(data_list, self.processed_paths[0])
 
+    def reads_dataset_factory(self, chromosome, transform=None):
+        class ArabidopsisReadsDataset(Dataset):
+            def __init__(self, root, transform):
+                self.root = root
+                self.transform = transform
+
+                self.reads_file = self.root + "/raw/chr" + str(chromosome) + "_reads.pkl"
+
+                with open(self.reads_file, 'rb') as f:
+                    self.reads_dict = pickle.load(f)
+
+            def __len__(self):
+                return len(self.reads_dict)
+
+            def __getitem__(self, idx):
+                sample = self.reads_dict[idx]
+
+                # Convert each sample read into one-hot encoded matrix
+                indices = []
+                for char in sample:
+                    match char:
+                        case 'A':
+                            indices.append(0)
+                        case 'T':
+                            indices.append(1)
+                        case 'C':
+                            indices.append(2)
+                        case 'G':
+                            indices.append(3)
+                sample = torch.nn.functional.one_hot(torch.tensor(indices), num_classes=4)
+
+                # Transform operates on one-hot encoded tensor
+                if self.transform:
+                    sample = self.transform(sample)
+
+                return sample
+
+        return ArabidopsisReadsDataset(self.root, transform)
+
+
 
 test = ArabidopsisDataset(root="./arabidopsis-dataset")
 print(test[0])
+print(test.reads_dataset_factory(3).__getitem__(0)[:10])

Removed batch normalization from SymGatedGCN, fixed RNN, and updated checkpoint analysis.

checkpoint_analysis.py
@@ -5,30 +5,57 @@ from dataset import Dataset, load_partitioned_dataset
 from lightning_modules import Model
 
 model = Model.load_from_checkpoint(
-    "lightning_logs/version_96/checkpoints/epoch=99-step=12800.ckpt",
+    "lightning_logs/version_22/checkpoints/epoch=29-step=3840.ckpt",
 )
 model.cpu()
-model.eval()
+# model.eval()
+
+def test_fn(subgraph):
+    prediction = model(subgraph)
+    print(f"Model output (unprocessed):")
+    print(prediction)
+    prediction = torch.nn.functional.sigmoid(prediction)
+    print(f"Model output (unprocessed 2):")
+    print(prediction)
+    post_processed = prediction.flatten()
+    print(f"Model output:")
+    print(post_processed)
+
+    print("Model predictions:")
+    print((post_processed < 0.1).nonzero().flatten())
+    print("Model prediction probabilities:")
+    print(post_processed[post_processed < 0.1])
+    print("True target:")
+    print((subgraph.edata["y"] == 0).nonzero().flatten())
+    print("Model prediction for true 0-edges:")
+    print(prediction[(subgraph.edata["y"] == 0).nonzero().flatten()])
+    if len((subgraph.edata["y"] == 0).nonzero().flatten()):
+        breakpoint()
 
 test_loader = DataLoader(
-    load_partitioned_dataset(Dataset.CHM13, 19, [14]),
+    load_partitioned_dataset(Dataset.CHM13, 19, [21]), # 21, 80
     batch_size=1,
     collate_fn=lambda single_graph_in_list: single_graph_in_list[0],
 )
-subgraph = next(iter(test_loader))
 torch.set_printoptions(threshold=10_000)
+subgraph = next(iter(test_loader))
+test_fn(subgraph)
+breakpoint()
 
-prediction = model(subgraph)
-prediction = torch.nn.functional.sigmoid(prediction)
-post_processed = prediction.flatten()
-print("Model output:")
-print(post_processed)
-
-print("Model predictions:")
-print((post_processed < 0.1).nonzero().flatten())
-print("Model prediction probabilities:")
-print(post_processed[post_processed < 0.1])
-print("True target:")
-print((subgraph.edata["y"] == 0).nonzero().flatten())
-print("Model prediction for true 0-edges:")
-print(prediction[(subgraph.edata["y"] == 0).nonzero().flatten()])
+test_loader = DataLoader(
+    load_partitioned_dataset(Dataset.CHM13, 19, [80]), # 21, 80
+    batch_size=1,
+    collate_fn=lambda single_graph_in_list: single_graph_in_list[0],
+)
+torch.set_printoptions(threshold=10_000)
+subgraph = next(iter(test_loader))
+test_fn(subgraph)
+# for i in range(128):
+#     test_loader = DataLoader(
+#         load_partitioned_dataset(Dataset.CHM13, 19, [i]),
+#         batch_size=1,
+#         collate_fn=lambda single_graph_in_list: single_graph_in_list[0],
+#     )
+#     for j, subgraph in enumerate(test_loader):
+#         print("testing {i=} {j=}")
+#         test_fn(subgraph)

layers/encoder.py
@@ -50,16 +50,20 @@ class NodeEdgeReadsEncoder(nn.Module):
         read_hidden = torch.zeros(
             (1, num_nodes, self.num_gru_features), device=x.device
         )
-        for i in range(0, seq_len, chunk_size):
-            chunk = read_data[:, i : i + chunk_size, :]
-            (_, read_hidden_residual) = self.gru(chunk, read_hidden)
-            read_hidden = read_hidden_residual + read_hidden
+        read_data_padds = read_data.sum(-1)
+        read_data_lens = read_data_padds.sum(-1).long()-1
+        arranged = torch.arange(read_data_lens.shape[0], device=read_data_lens.device)
+        (rh, rho) = self.gru(read_data, read_hidden)
+        # for i in range(0, seq_len, chunk_size):
+        #     chunk = read_data[:, i : i + chunk_size, :]
+        #     (_, read_hidden_residual) = self.gru(chunk, read_hidden)
+        #     read_hidden = read_hidden_residual + read_hidden
 
         # Remove GRU's D * num_layers dimension
         # https://pytorch.org/docs/stable/generated/torch.nn.GRU.html#torch.nn.GRU
-        read_hidden = torch.squeeze(read_hidden)
+        # read_hidden = torch.squeeze(read_hidden)
+        read_hidden = rh[arranged, read_data_lens, :]
         read_hidden = self.gru_expander(read_hidden)
-        breakpoint()
 
         x = torch.concatenate([x, read_hidden], dim=-1)
 

layers/gated_gcn_full.py
@@ -36,6 +36,8 @@ class SymGatedGCN(nn.Module):
         self.B_3 = nn.Linear(in_channels, out_channels, dtype=dtype)
 
         if batch_norm: # batch normalization
+            # self.bn_h = nn.BatchNorm1d(out_channels, track_running_stats=True)
+            # self.bn_e = nn.BatchNorm1d(out_channels, track_running_stats=True)
             self.bn_h = nn.BatchNorm1d(out_channels, track_running_stats=True)
             self.bn_e = nn.BatchNorm1d(out_channels, track_running_stats=True)
         else: # layer normalization
@@ -104,7 +106,7 @@ class SymGatedGCN(nn.Module):
             # Forward-message passing
             g.apply_edges(fn.u_add_v('B1h', 'B2h', 'B12h'))
             e_ji = g.edata['B12h'] + g.edata['B3e']
-            e_ji = self.bn_e(e_ji)
+            # e_ji = self.bn_e(e_ji)
             e_ji = F.relu(e_ji)
             if self.residual:
                 e_ji = e_ji + e_in
@@ -117,7 +119,7 @@ class SymGatedGCN(nn.Module):
             # Backward-message passing
             g_reverse.apply_edges(fn.u_add_v('B2h', 'B1h', 'B21h'))
             e_ik = g_reverse.edata['B21h'] + g_reverse.edata['B3e']
-            e_ik = self.bn_e(e_ik)
+            # e_ik = self.bn_e(e_ik)
             e_ik = F.relu(e_ik)
             if self.residual:
                 e_ik = e_ik + e_in
@@ -173,8 +175,10 @@ class GatedGCN(nn.Module):
         self.B_3 = nn.Linear(in_channels, out_channels, dtype=dtype)
 
         if batch_norm: # batch normalization
-            self.bn_h = nn.BatchNorm1d(out_channels, track_running_stats=True)
-            self.bn_e = nn.BatchNorm1d(out_channels, track_running_stats=True)
+            # self.bn_h = nn.BatchNorm1d(out_channels, track_running_stats=True)
+            # self.bn_e = nn.BatchNorm1d(out_channels, track_running_stats=True)
+            self.bn_h = nn.Identity(out_channels, track_running_stats=True)
+            self.bn_e = nn.Identity(out_channels, track_running_stats=True)
         else: # layer normalization
             self.bn_h = nn.LayerNorm(out_channels) 
             self.bn_e = nn.LayerNorm(out_channels) 
@@ -223,7 +227,7 @@ class GatedGCN(nn.Module):
             if self.residual:
                 h = h + h_in
 
-            h = F.dropout(h, self.dropout, training=self.training)
+            # h = F.dropout(h, self.dropout, training=self.training)
             e = g.edata['e_ji']
 
             return h, e

lightning_modules.py
@@ -21,15 +21,15 @@ class TrainingConfig:
     num_layers = 8
     num_hidden_edge_scores = 64
     batch_norm = True
-    pos_weight = 0.008
+    pos_weight = 1/0.008
 
 
 def symmetry_loss(org_scores, rev_scores, labels, pos_weight=1.0, alpha=1.0):
     BCE = torch.nn.BCEWithLogitsLoss(
         pos_weight=torch.tensor(pos_weight), reduction="none"
     )
-    BCE_org = BCE(org_scores, labels)
-    BCE_rev = BCE(rev_scores, labels)
+    BCE_org = BCE(-org_scores, 1-labels)
+    BCE_rev = BCE(-rev_scores, 1-labels)
     abs_diff = torch.abs(org_scores - rev_scores)
     loss = BCE_org + BCE_rev + alpha * abs_diff
     loss = loss.mean()
@@ -82,12 +82,15 @@ class Model(L.LightningModule):
             edge_predictions=org_scores, edge_labels=labels
         )
         accuracy, precision, recall, f1 = utils.calculate_metrics(TP, TN, FP, FN)
+        if (labels == 0).any():
+            print(org_scores[labels == 0])
+            print(rev_scores[labels == 0])
 
         self.log("train_loss", loss, prog_bar=True)
 
-        self.log("train_accuracy", accuracy, prog_bar=True)
-        self.log("train_precision", precision, prog_bar=True)
-        self.log("train_recall", recall, prog_bar=True)
+        # self.log("train_accuracy", accuracy, prog_bar=True)
+        # self.log("train_precision", precision, prog_bar=True)
+        # self.log("train_recall", recall, prog_bar=True)
         self.log("train_f1", f1, prog_bar=True)
 
         self.log("TP", TP, prog_bar=True)

models.py
@@ -71,7 +71,7 @@ class SymGatedGCNWithReadsModel(nn.Module):
             num_edge_features,
             num_intermediate_hidden_features,
             num_hidden_features,
-            num_gru_features=8,
+            num_gru_features=32,
         )
         self.gnn = layers.SymGatedGCN_processor(
             num_layers, num_hidden_features, batch_norm, dropout=dropout
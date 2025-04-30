b28ff77526fbc7e8c6ceb1cb7e24a3cfa5e8bc65
Yash Shah <yashshah00@outlook.com>
2025-04-30 23:28:19 +0100

    Added SymGAT.

--- models.py
+++ models.py
@@ -106,6 +106,53 @@ class GATModel(nn.Module):
         scores = self.predictor(graph, x, e)
         return scores
 
+
+class SymGATModel(nn.Module):
+    def __init__(
+        self,
+        num_node_features,
+        num_edge_features,
+        num_intermediate_hidden_features,
+        num_hidden_features,
+        num_layers,
+        num_hidden_edge_scores,
+        batch_norm,
+        dropout=None,
+        use_cuda=True,
+    ):
+        super().__init__()
+        self.linear1_node = nn.Linear(
+            num_node_features, num_intermediate_hidden_features
+        )
+        self.linear2_node = nn.Linear(
+            num_intermediate_hidden_features, num_hidden_features
+        )
+        self.linear1_edge = nn.Linear(
+            num_edge_features, num_intermediate_hidden_features
+        )
+        self.linear2_edge = nn.Linear(
+            num_intermediate_hidden_features, num_hidden_features
+        )
+        self.gnn = layers.SymGAT_processor(
+            num_layers, num_hidden_features, batch_norm, dropout=dropout
+        )
+        self.predictor = layers.ScorePredictor(
+            num_hidden_features * 3, num_hidden_edge_scores
+        )
+
+    def forward(self, graph, x, e):
+        x = self.linear1_node(x)
+        x = torch.relu(x)
+        x = self.linear2_node(x)
+
+        e = self.linear1_edge(e)
+        e = torch.relu(e)
+        e = self.linear2_edge(e)
+
+        x, e = self.gnn(graph, x, e)
+        scores = self.predictor(graph, x, e)
+        return scores
+
 class SymGatedGCNWithReadsModel(nn.Module):
     def __init__(
         self,
@@ -333,6 +380,7 @@ class SymGatedGCNRandomEdgeModel(nn.Module):
 class ModelType(Enum):
     SymGatedGCN = SymGatedGCNModel
     GAT = GATModel
+    SymGAT = SymGATModel
     SymGatedGCNReads = SymGatedGCNWithReadsModel
     SymGatedGCNMamba = SymGatedGCNMambaModel
     SymGatedGCNMambaOnly = SymGatedGCNMambaOnlyModel
